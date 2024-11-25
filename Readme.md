df12db355651985efc7091524c5647a404bee01a
Saket Chaudhary <saketchaudharynit@gmail.com>
2024-11-25 23:17:56 +0530

    init

---
+++ Readme.md
@@ -0,0 +1,2 @@
+Steps mentioned here https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
+
---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+from aws_xray_sdk.core import xray_recorder
+
+def lambda_handler(event, context):
+    # ... some code
+
+    subsegment = xray_recorder.begin_subsegment('subsegment_name')
+    # Code to record
+    # Add metadata or annotation here, if necessary
+    subsegment.put_metadata('key', dict, 'namespace')
+    subsegment.put_annotation('key', 'value')
+
+    xray_recorder.end_subsegment()
+
+    # ... some other code
+    return True
